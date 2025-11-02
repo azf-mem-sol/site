01304d987fa31a9fc015ed3c13556374973e7832
azf-mem-sol <webmaster@azf-memoireetsolidarite.com>
2025-11-02 18:35:37 +0100

    Ajout du script etat-actuel.bat pour vérifier l'état du site

---
+++ _état-actuel.bat
@@ -0,0 +1,24 @@
+:: /--------------------------------------------------------------------------\
+:: |                                                                          |
+:: |                                                                          |
+:: |                      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                      |
+:: |                      !!!!!                    !!!!!                      |
+:: |                      !!!!!      STOP !!!!     !!!!!                      |
+:: |                      !!!!!                    !!!!!                      |
+:: |                      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!                      |
+:: |                                                                          |
+:: |                                                                          |
+:: |     Tu as ouvert le script _enregistrer.bat. Il ne faut surtout pas      |
+:: |     modifier ce fichier — sinon, ce script ne marchera plus ! Si tu      |
+:: |        voulais exécuter (et non modifier) le script, ferme cette         |
+:: |        fenêtre et double-clique sur le fichier _enregistrer.bat.         |
+:: |                                                                          |
+:: \--------------------------------------------------------------------------/
+
+:: Set echo to off, and set the code page to UTF-8.
+@echo off
+chcp 65001 > nul
+
+git status
+pause
+
